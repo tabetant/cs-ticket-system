dc0bbf0cfb4f98dc6de5ad516f50ec1ab1e73639
tabetant <antoine.tabet@mail.utoronto.ca>
2025-06-09 10:28:59 +0300

    implemented password reset form and page

---
+++ src/app/ui/PasswordResetForm.tsx
@@ -0,0 +1,47 @@
+'use client'
+import { supabase } from "@/db/client";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useRouter, usePathname } from "next/navigation";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+
+export default function PasswordResetForm() {
+    const router = useRouter();
+    const inputsSchema = z.object({
+        password: z.string().min(6, "Password must be at least 6 characters long"),
+    })
+    type Inputs = z.infer<typeof inputsSchema>;
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Inputs>({
+        resolver: zodResolver(inputsSchema),
+        defaultValues: {
+            password: '',
+        }
+    })
+    const onSubmit: SubmitHandler<Inputs> = async (data) => {
+        const { error } = await supabase().auth.updateUser({ password: data.password })
+        if (error) {
+            console.error("Error updating password:", error);
+        } else {
+            console.log("Password updated successfully:", data);
+            router.push('/login');
+        }
+    }
+
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <h1>Enter new password</h1>
+            <div>
+                <label>Password:</label>
+                <input type="password" {...register("password")} />
+                {errors.password && <p>{errors.password.message}</p>}
+            </div>
+            <button type="submit">Reset Password</button>
+        </form>
+    );
+}
