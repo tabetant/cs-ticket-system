64a397d6ef367f140ef58d28b8c23f6cb7a19ded
tabetant <antoine.tabet@mail.utoronto.ca>
2025-06-09 11:53:47 +0300

    draft ticket sub form

---
+++ src/app/public/ticket-submission/page.tsx
@@ -0,0 +1,12 @@
+
+export default function TicketSubmissionPage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+                <h1 className="text-2xl font-bold mb-6 text-center">Create a Ticket</h1>
+                <p className="text-center">Please fill out the form below to submit a ticket.</p>
+                {/* Form for ticket submission will go here */}
+            </div>
+        </div>
+    );
+}
---
+++ src/app/ui/TicketSubmissionForm.tsx
@@ -0,0 +1,50 @@
+'use client'
+import { supabase } from "@/db/client";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useRouter, usePathname } from "next/navigation";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+export default function SignupForm() {
+    const router = useRouter();
+    const pathname = usePathname();
+    const inputsSchema = z.object({
+        title: z.string().min(1, "Title is required"),
+        description: z.string().min(1, "Description is required"),
+    })
+    type Inputs = z.infer<typeof inputsSchema>;
+    const {
+        register,
+        watch,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Inputs>({
+        resolver: zodResolver(inputsSchema),
+        defaultValues: {
+            title: "",
+            description: "",
+        }
+    })
+
+    const onSubmit: SubmitHandler<Inputs> = async (data) => {
+
+    }
+
+    return (
+        <form>
+            <h1>New Ticket</h1>
+            <div>
+                <label>Title:</label>
+                <input type="text" {...register("title")} />
+                {errors.title && <p>{errors.title.message}</p>}
+            </div>
+            <div>
+                <label>Description:</label>
+                <input type="text" {...register("description")} />
+                {errors.description && <p>{errors.description.message}</p>}
+            </div>
+
+            <button type="submit" onClick={handleSubmit(onSubmit)}>Add Ticket</button>
+        </form>
+    )
+}
